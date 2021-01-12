b2381bda64985d07fc9e5e4315f5a38dca5c4163
Kirill Nadelyaev <k.nadelyaev17@gmail.com>
2021-01-12 11:14:27 +0200

    init

---
+++ specs/Migrations/202101120411412_init.cs
@@ -0,0 +1,46 @@
+﻿namespace specs.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.City",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Latitude = c.Int(nullable: false),
+                        Longitude = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Employee",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 128),
+                        CityId = c.Int(nullable: false),
+                        Specialization = c.String(),
+                        FavoriteVideogame = c.String(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.City", t => t.CityId, cascadeDelete: true)
+                .Index(t => t.CityId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employee", "CityId", "dbo.City");
+            DropIndex("dbo.Employee", new[] { "CityId" });
+            DropTable("dbo.Employee");
+            DropTable("dbo.City");
+        }
+    }
+}
---
+++ specs/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace ExamPractice
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CityId { get; set; }
+        public City City { get; set; }
+    }
+    public class Doctor : Employee
+    {
+        public string Specialization { get; set; }
+    }
+    public class Engineer : Employee
+    {
+        public string FavoriteVideogame { get; set; }
+    }
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+    public class Context : DbContext
+    {
+        public Context() : base("ef2")
+        { }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Engineer> Engineers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Employee>().HasRequired(x => x.City).WithMany(x => x.Employees).HasForeignKey(x => x.CityId);
+
+            modelBuilder.Entity<Doctor>().Property(d => d.Specialization).IsRequired();
+            modelBuilder.Entity<Employee>().Property(e => e.Name).HasMaxLength(128);
+        }
+    }
+
+
+    class Program
+    {
+        public static IEnumerable<string> GetSpecialization(int cityId)
+        {
+            using (var ctx = new Context())
+            {
+                return ctx.Doctors.Where(d => d.CityId == cityId).Select(d => d.Specialization).ToList();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //var ctx = new Context();
+            //ctx.Cities.Add(new City { Name = "Kiev", Latitude = 1, Longitude = 1 });
+            //ctx.Cities.Add(new City { Name = "Kiev1", Latitude = 1, Longitude = 1 });
+
+            //ctx.Doctors.Add(new Doctor { CityId = 1, Name = "name1", Specialization = "spec1" });
+            //ctx.Doctors.Add(new Doctor { CityId = 1, Name = "name2", Specialization = "spec2" });
+            //ctx.Doctors.Add(new Doctor { CityId = 1, Name = "name3", Specialization = "spec3" });
+            //ctx.Doctors.Add(new Doctor { CityId = 1, Name = "name4", Specialization = "spec4" });
+
+            //ctx.Engineers.Add(new Engineer { CityId = 1, Name = "name4", FavoriteVideogame = "game1" });
+
+            //ctx.SaveChanges();
+
+            foreach (var sp in GetSpecialization(1))
+            {
+                Console.WriteLine(sp);
+            }
+            Console.ReadLine();
+        }
+    }
+}
